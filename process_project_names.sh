d5c22d7b77d076ab38c49d016efc65692eb11614
bellicoseren <www.renehr@gmail.com>
2021-10-26 08:10:47 -0500

    Update process_project_names.sh

--- process_project_names.sh
+++ process_project_names.sh
@@ -3,10 +3,8 @@
 while read line 
 do
  echo $line ----------;
- echo "curl -H 'authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=' -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames$line.json"
 
- somevariable="$(curl -H 'authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=' -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames$line.json)";
+ curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$line/repositories" -o repositoriesnames$line.json
                  
- echo $somevariable;
  cat repositoriesnames$line.json
 done < projectlist.txt;
