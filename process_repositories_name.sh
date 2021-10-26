35452f3155f68f50fbb48e3b8246dba955f81d28
bellicoseren <www.renehr@gmail.com>
2021-10-26 08:45:27 -0500

    Update process_repositories_name.sh

--- process_repositories_name.sh
+++ process_repositories_name.sh
@@ -9,6 +9,6 @@ do
  # /projects/{project_name}/repositories/{repository_name}/artifacts
  cat artifactname$project_$repository.json | jq '.[].name' > artifact$project_$repositorylist.txt
  sed -i 's/"//g' artifactname$project_$repository.json
- # cat repositoriesnames$line.json
- # cat repository$linelist.txt
+ cat artifactname$project_$repository.json
+ cat artifact$project_$repositorylist.txt
 done < $1;
