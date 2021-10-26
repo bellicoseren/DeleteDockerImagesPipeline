f20b3f3beed14334c5e6a91d7d21f2d762588585
bellicoseren <www.renehr@gmail.com>
2021-10-26 08:47:14 -0500

    Update process_project_names.sh

--- process_project_names.sh
+++ process_project_names.sh
@@ -7,7 +7,7 @@ do
  curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$line/repositories" -o repositoriesnames$line.json
  cat repositoriesnames$line.json | jq '.[].name' > repository$linelist.txt
  sed -i 's/"//g' repository$linelist.txt
- cat repositoriesnames$line.json
+ cat repositoriesnames$line.json | jq
  cat repository$linelist.txt
  ./process_repositories_name.sh repository$linelist.txt
 done < projectlist.txt;
