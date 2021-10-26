4e1f61473c16c3e3d0996bad690a0b9447d321c4
bellicoseren <www.renehr@gmail.com>
2021-10-26 08:39:33 -0500

    Update process_repositories_name.sh

--- process_repositories_name.sh
+++ process_repositories_name.sh
@@ -5,9 +5,9 @@ do
  echo $line ----------;
  project=$(echo $line | cut -d / -f 1;)
  repository=$(echo $line | cut -d / -f 2;)
- curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$line/repositories" -o repositoriesnames$line.json
- cat repositoriesnames$line.json | jq '.[].name' > repository$linelist.txt
+ curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$line/repositories" -o artifactname$project_$repository.json
+ # cat repositoriesnames$line.json | jq '.[].name' > repository$linelist.txt
  sed -i 's/"//g' repository$linelist.txt
- cat repositoriesnames$line.json
- cat repository$linelist.txt
+ # cat repositoriesnames$line.json
+ # cat repository$linelist.txt
 done < $1;
