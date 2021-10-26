7898a4fe38c4f54ed013ec4f386eec5a14c990a3
bellicoseren <www.renehr@gmail.com>
2021-10-25 19:17:22 -0500

    Update process_project_names.sh

--- process_project_names.sh
+++ process_project_names.sh
@@ -3,9 +3,9 @@
 while read line 
 do
  echo $line ----------;
- echo "curl -u admin:Harbor12345 -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames.json"
+ echo "curl -u admin:Harbor12345 -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames$line.json"
 
- somevariable="$(curl -u admin:Harbor12345 -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames.json)";
+ somevariable="$(curl -u admin:Harbor12345 -sS -X GET 'http://172.18.0.1/api/v2.0/projects/$line/repositories' -o repositoriesnames$line.json)";
  echo $somevariable;
- 
+ cat repositoriesnames$line.json
 done < projectlist.txt;
