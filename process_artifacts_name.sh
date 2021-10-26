25e93224f84232ba5a850601b31d8d54820c38a8
bellicoseren <www.renehr@gmail.com>
2021-10-26 11:19:41 -0500

    Update process_artifacts_name.sh

--- process_artifacts_name.sh
+++ process_artifacts_name.sh
@@ -16,6 +16,15 @@ do
    repository=$(echo $url | cut -d / -f 2;)
  else
    tag=$(echo $line | cut -d / -f 3;)
+   
+   curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$project/repositories/$repository/artifacts/$tag?page=1&page_size=10&with_tag=true&with_label=true&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -o tag$project$repository$tag.json
+   # curl -X GET "http://localhost/api/v2.0/projects/cicd/repositories/jenkins/artifacts/1.0?page=1&page_size=10&with_tag=true&with_label=false&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -H "X-Accept-Vulnerabilities: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU="
+   # curl -X GET "http://localhost/api/v2.0/projects/test/repositories/vault/artifacts?page=1&page_size=10&with_tag=true&with_label=true&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -H "X-Accept-Vulnerabilities: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU="
+   # /projects/{project_name}/repositories/{repository_name}/artifacts/{reference}
+   cat tag$project$repository$tag.json | jq '.[].tags[].name' > tag$project$repository$tag.txt
+   sed -i 's/"//g' tag$project$repository$tag.txt
+   cat tag$project$repository$tag.json | jq
+   cat tag$project$repository$tag.txt
  fi
  
  # url=$(echo $line | cut -d / -f 5,7;)
@@ -23,14 +32,7 @@ do
  # repository=$(echo $url | cut -d / -f 2;)
  # tag=$(echo $line | cut -d / -f 3;)
  
- curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$project/repositories/$repository/artifacts/$tag?page=1&page_size=10&with_tag=true&with_label=true&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -o tag$project$repository$tag.json
- # curl -X GET "http://localhost/api/v2.0/projects/cicd/repositories/jenkins/artifacts/1.0?page=1&page_size=10&with_tag=true&with_label=false&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -H "X-Accept-Vulnerabilities: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU="
- # curl -X GET "http://localhost/api/v2.0/projects/test/repositories/vault/artifacts?page=1&page_size=10&with_tag=true&with_label=true&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -H "X-Accept-Vulnerabilities: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU="
- # /projects/{project_name}/repositories/{repository_name}/artifacts/{reference}
- cat tag$project$repository$tag.json | jq '.[].tags[].name' > tag$project$repository$tag.txt
- sed -i 's/"//g' tag$project$repository$tag.txt
- cat tag$project$repository$tag.json | jq
- cat tag$project$repository$tag.txt
+ 
  
  count+=1;
  
