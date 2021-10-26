07c6ec3ab016f71c5b72c9ec54567b3efa4d042c
bellicoseren <www.renehr@gmail.com>
2021-10-26 10:39:05 -0500

    Update process_artifacts_name.sh

--- process_artifacts_name.sh
+++ process_artifacts_name.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+ count=0;
+
+
+
+
 while read line 
 do
+
  echo "+++ Checking artifacts $line +++";
- project=$(echo $line | cut -d / -f 1;)
- repository=$(echo $line | cut -d / -f 2;)
- tag=$(echo $line | cut -d / -f 3;)
+ if[[count==0]] then
+   url=$(echo $line | cut -d / -f 5,7;)
+   project=$(echo $url | cut -d / -f 1;)
+   repository=$(echo $url | cut -d / -f 2;)
+ else
+   tag=$(echo $line | cut -d / -f 3;)
+ fi
+ 
+ # url=$(echo $line | cut -d / -f 5,7;)
+ # project=$(echo $url | cut -d / -f 1;)
+ # repository=$(echo $url | cut -d / -f 2;)
+ # tag=$(echo $line | cut -d / -f 3;)
  
  curl -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU=" -sS -X GET "http://172.18.0.1/api/v2.0/projects/$project/repositories/$repository/artifacts/$tag?page=1&page_size=10&with_tag=true&with_label=true&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -o tag$project$repository$tag.json
  # curl -X GET "http://localhost/api/v2.0/projects/cicd/repositories/jenkins/artifacts/1.0?page=1&page_size=10&with_tag=true&with_label=false&with_scan_overview=false&with_signature=false&with_immutable_status=false" -H "accept: application/json" -H "X-Accept-Vulnerabilities: application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0" -H "authorization: Basic YWRtaW46SGFyYm9yMTIzNDU="
@@ -15,4 +30,5 @@ do
  # sed -i 's/"//g' artifact$project$repositorylist.txt
  # cat artifactname$project$repository.json | jq
  # cat artifact$project$repositorylist.txt
+ count=count+1;
 done < $1;
